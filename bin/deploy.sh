48afea2d2f8016142079dff4a5387f25434a2808
iain <iain@soulflyer.co.uk>
2016-05-03 17:56:43 +0700

    rename deploy

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,8 +2,8 @@ echo "Copying files to soulflyer"
 rsync -vaikL  --exclude=.DS_Store --exclude=.sass-cache --exclude=.git \
     --exclude=zip --exclude=json \
     /Users/iain/Code/soulflyer-new/albums \
-        jphuquoc@soulflyer.co.uk:albums/
+        jphuquoc@soulflyer.co.uk:
 rsync -vaikL  --exclude=.DS_Store --exclude=.sass-cache --exclude=.git \
       --exclude=zip --exclude=json \
       /Users/iain/Code/soulflyer-new/divecentres \
-      jphuquoc@soulflyer.co.uk:divecentres/
+      jphuquoc@soulflyer.co.uk:
