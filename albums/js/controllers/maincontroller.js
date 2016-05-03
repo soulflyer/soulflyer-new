fe3ff92e635e3953ee64df5641fbb89104ee0eca
iain <iain@soulflyer.co.uk>
2016-05-03 12:02:28 +0700

    tidied some staff details

--- albums/js/controllers/maincontroller.js
+++ albums/js/controllers/maincontroller.js
@@ -2,9 +2,9 @@ albums.controller(
     "MainController",
     function ($scope) {
         $scope.rootURL = "http://soulflyer.co.uk/photos";
-        $scope.commission = 10;
-        $scope.price = 60;
-        $scope.discountPrice = 45;
-        $scope.lowestPrice = 30;
+        $scope.commission = "200,000VND";
+        $scope.price = "1,500,000VND";
+        $scope.discountPrice = "1,200,000VND";
+        $scope.lowestPrice = "1,000,000VND";
         $scope.phone = "093 756 9354";
     });
